9e113fa3b4f1900471c46ad1c2a55241c4cb4f9e
tilekbai <kukushokk@gmail.com>
2024-01-12 20:37:13 +0600

    Project creation: weather_api

---
+++ weather_api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('weather.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
